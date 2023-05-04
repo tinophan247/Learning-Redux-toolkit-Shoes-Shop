6463ca09ad6c9b069de0d8380d63505f77cf010b
Tino Phan <tinophan247@gmail.com>
2023-05-04 15:40:40 +0700

    Learning Redux Toolkit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Products from './pages/Products/Products';
+
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
